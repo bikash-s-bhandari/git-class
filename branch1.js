e8f1c2751ff1377d9f2888721ecbe8522b0948af
Bikash Bhandari <bikash.bhandari05@gmail.com>
2022-02-08 17:12:49 +0545

    stash file added

--- branch1.js
+++ branch1.js
@@ -1,1 +1,2 @@
 console.log("branch 1 is created here");
+console.log("m stash here");
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log("hello world");
 console.log("my name is bikash", "i love programming");
+console.log("new changes for stash");
